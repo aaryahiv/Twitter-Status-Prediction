c4406878fbab773b877126bc65f62a02d85bf1e4
Totaly not Ben <jazzy4827@gmail.com>
2019-03-31 07:15:42 -0700

    a training data parser

--- src/Program/Main.py
+++ src/Program/Main.py
@@ -1,3 +1,29 @@
+import time
+
+from Object import TrainingData
+
+with open('sample.txt', 'r') as trainingDataFile:
+  rawData = trainingDataFile.read()
+
+list = TrainingData.rawDataToList(rawData, " eatshit ")
+
+print(list)
+time.sleep(2)
+print(TrainingData.getContent(list[0]))
+print(TrainingData.getFollowers(list[0]))
+print(TrainingData.getPopularity(list[0]))
+
+json = TrainingData.parseTrainingSampleToJson(list[0])
+
+print(json)
+
+jsonlist = TrainingData.parseListToJsonList(list)
+
+print(jsonlist)
+time.sleep(2)
+print(jsonlist[0])
+print(jsonlist[420])
+print(jsonlist[69].get("input").get("content"))
 
 def main():
     f = open('sample.txt')
